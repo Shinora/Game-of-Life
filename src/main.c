9eb08736646d356b8018a3fb96acebc388e41ca7
Simon AMATO <simonamato23@gmail.com>
2020-06-25 01:02:54 +0200

    working hardcoded life game

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include  <unistd.h>
 #include "../include/main.h"
 
 
@@ -10,31 +11,106 @@ int main(){
 
     printf(" Welcome to the game of life, please enter a number to define the bi-dimmensional array size : ");
     scanf("%10d", &size);
+    if(size >1000){ exit(1);}
 
-    char grid[size][size];
+    int grid[size][size];
+    int buffer [size][size];
 
-    init_state(**grid, size);
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
+
+            grid[i][j] = random_bool();
+
+        }
+        printf("\n");
+        
+    }
+    printf("\n");
+    printf("\n");
+    printf("\n");
+
+    while(1){
+
+        for(int i=0; i<size; i++){
+            for(int j=0; j<size; j++){
+                int ne = 0;
+
+                if(grid[i+1][j] == 1){
+                    ne+=1;
+                }
+                if(grid[i+1][j+1] == 1){
+                    ne+=1;
+                }
+                if(grid[i+1][j-1] == 1){
+                    ne+=1;
+                }
+                if(grid[i][j+1] == 1){
+                    ne+=1;
+                }
+                if(grid[i][j-1] == 1){
+                    ne+=1;
+                }
+                if(grid[i-1][j] == 1){
+                    ne+=1;
+                }
+                if(grid[i-1][j+1] == 1){
+                    ne+=1;
+                }
+                if(grid[i-1][j-1] == 1){
+                    ne+=1;
+                }
+
+                if(grid[i][j] == 1){
+                    if(ne<2 || ne>3){
+                        buffer[i][j] = 0;
+                    }
+                    else if(ne == 2 || ne == 3){
+                        buffer[i][j] = 1;
+                    }
+                }
+
+                else{
+                    if(ne==3){
+                        buffer[i][j] = 1;
+                    }
+                    else{
+                        buffer[i][j] = 0;
+                    }
+                }
+                
+
+            }
+        }
+        for(int k=0; k<size; k++){
+            for(int l=0; l<size; l++){
+                grid[k][l] = buffer[k][l];
+                printf("%d", grid[k][l]);
+
+            }
+            printf("\n");
+        }
+        sleep(1);
+        printf("\n");
+        printf("\n");
+        printf("\n");
+    }
+
+
+    
+
 
     return 0;
   
 }
 
-void init_state(char **pointeuronarray,int size){
-
-	for(int i=0; i<50; i++){
-        random_bool();
-		
-	}
-
-}
 
 int random_bool(){
 
     int randomdigit = 0;
 	 // initialisation de rand
 	
-	randomdigit = rand();
-	printf("%d ",randomdigit);
+	randomdigit = rand()%2;
+	printf("%d",randomdigit);
 
     return randomdigit;
 	
